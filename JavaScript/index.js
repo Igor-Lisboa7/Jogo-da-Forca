dad93a310d677e902a6b2d1cd7d4ba9ca777bbe8
Igor Lisboa <igorresendelisboa1@gmail.com>
2023-02-13 10:19:08 -0300

    Atualização de Imagem bug entre outros bugs agora vai numero 5 agora vai  do  Jogo da Forca...

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -680,25 +680,25 @@ async function atraso(tempo){
 function carregaImagemForca(){
     switch(tentativas){
         case 5:
-        document.getElementById("imagem").style.background = "url('https://raw.githubusercontent.com/agnaldoguima/jogoDaForca/master/img/forca01.png')";
+        document.getElementById("imagem").style.background = "url('./Img/forca01.png')";
         break;
     case 4:
-        document.getElementById("imagem").style.background = "url('https://github.com/agnaldoguima/jogoDaForca/blob/master/img/forca02.png')";
+        document.getElementById("imagem").style.background = "url('./Img/forca02.png')";
         break;
     case 3:
-        document.getElementById("imagem").style.background = "url('https://raw.githubusercontent.com/agnaldoguima/jogoDaForca/master/img/forca03.png')";
+        document.getElementById("imagem").style.background = "url('./Img/forca03.png')";
         break;
     case 2:
-        document.getElementById("imagem").style.background = "url('https://raw.githubusercontent.com/agnaldoguima/jogoDaForca/master/img/forca04.png')";
+        document.getElementById("imagem").style.background = "url('./Img/forca04.png')";
         break;
     case 1:
-        document.getElementById("imagem").style.background = "url('https://raw.githubusercontent.com/agnaldoguima/jogoDaForca/master/img/forca05.png')";
+        document.getElementById("imagem").style.background = "url('./Img/forca05.png')";
         break;
     case 0:
-        document.getElementById("imagem").style.background = "url('https://raw.githubusercontent.com/agnaldoguima/jogoDaForca/master/img/forca06.png')";
+        document.getElementById("imagem").style.background = "url('./Img/forca06.png')";
         break;
     default:
-        document.getElementById("imagem").style.background = "url('https://raw.githubusercontent.com/agnaldoguima/jogoDaForca/master/img/forca.png')";
+        document.getElementById("imagem").style.background = "url('./Img/forca.png')";
 
     }
     
